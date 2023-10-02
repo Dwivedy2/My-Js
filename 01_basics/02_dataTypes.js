a71be82829c20251d494573e07b496ad5f6d7d06
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2023-10-02 09:37:00 +0000

    Data Types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // this is nodejs, not browser dom
+
+/*
+    JS documents
+    tc39
+    mdn
+*/
+
+// Data Types
+
+let age = 18
+let name = "Omkareshwar"
+let isloggedIn = false
+
+// Primitive
+
+// number => 2 to the power 53
+// bigint
+// string
+// boolean
+// undefined => value is not assigned
+// null => empty, not recived any response
+// symbol => unique
+
+// User Defined
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
