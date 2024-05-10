9ae90640d926df3ca19dc02231ed907331b1547f
Omkareshwar Nath Dwivedy <omkareshwarnath.dwivedy.sbg@gmail.com>
2024-05-10 10:18:43 +0530

    max_min

--- DSA/BasicIO/functions.js
+++ DSA/BasicIO/functions.js
@@ -4,8 +4,19 @@ function sayHello(name = "") {
     console.log(`Hello! ${name}`);
 };
 
-sayHello();
+// sayHello();
 
-let name = readline.question();
+// let name = readline.question();
+// sayHello(name);
 
-sayHello(name);
+// max, min
+// console.log((Math.max(2, 3))); // 3
+// console.log(Math.min(2, 3)); // 2
+console.log(maxx(2, 3))
+
+function maxx(n1, n2) {
+    if(n1 > n2) {
+        return n1;
+    }
+    return n2;
+}
