0067154d934c6e3d3c2cd91a5e6dbdba56200a73
Omkareshwar Nath Dwivedy <omkareshwarnath.dwivedy.sbg@gmail.com>
2024-06-03 08:26:33 +0530

    Chrome Extention: upgraded version

--- Projects/ChromeExtention/content.js
+++ Projects/ChromeExtention/content.js
@@ -1,9 +1,18 @@
 (function() {
     const currentUrl = window.location.href;
-    const githubUrlPattern = /^https:\/\/github\.com\/(.+)/;
-
-    if (githubUrlPattern.test(currentUrl)) {
-        const newUrl = currentUrl.replace(githubUrlPattern, 'https://github.dev/$1');
-        window.location.replace(newUrl);
+    if(currentUrl.includes('.com')) {
+        const githubUrlPattern = /^https:\/\/github\.com\/(.+)/;
+    
+        if (githubUrlPattern.test(currentUrl)) {
+            const newUrl = currentUrl.replace(githubUrlPattern, 'https://github.dev/$1');
+            window.location.replace(newUrl);
+        }
+    } else {
+        const githubUrlPattern = /^https:\/\/github\.dev\/(.+)/;
+    
+        if (githubUrlPattern.test(currentUrl)) {
+            const newUrl = currentUrl.replace(githubUrlPattern, 'https://github.com/$1');
+            window.location.replace(newUrl);
+        }
     }
 })();
