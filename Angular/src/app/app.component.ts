8d31c7b3d25eac0e95a4db9b65824307fbb14377
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-03-12 16:05:50 +0000

    Day2: Signals & Computed

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular';
+  title = 'Step';
+  
+  dailyStepGoal = signal(0);
+  step = signal(0);
+  
+  increaseStep() {
+    this.step.set(this.step() + 1);
+  }
+  
+  resetStep() {
+    this.step.set(0);
+  }
+
+  // computed do not modify but depend on signal.
+  progress = computed(() => {
+    return this.dailyStepGoal() > 0 ? (this.step() / this.dailyStepGoal()) * 100 : 0;
+  });
+
+  takeInput(event: any) {
+    this.dailyStepGoal.set(event.value);
+  }
 }
