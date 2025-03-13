88f1e583a65c72e24110f9d806348895c5a1af19
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-03-13 09:34:40 +0000

    Day3: Added Interfaces with Templates

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component, signal, computed } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, signal, computed, Signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Step';
   
   dailyStepGoal = signal(0);
   step = signal(0);
+  isUserRegistered = signal(false);
+  showInitialMessage = signal(false);
+  isGoalSet = signal(false);
+  showCompletionMessage = signal(false);
+  dailyTargets: number[] = [];
   
   increaseStep() {
     this.step.set(this.step() + 1);
+    if(this.progress() === 100) {
+      this.showCompletionMessage.set(true);
+      
+      setTimeout(() => {
+        this.showCompletionMessage.set(false);
+      }, 5000);
+    }
   }
   
   resetStep() {
@@ -27,6 +40,22 @@ export class AppComponent {
   });
 
   takeInput(event: any) {
-    this.dailyStepGoal.set(event.value);
+    this.dailyStepGoal.set(event.value ?? 0);
+  }
+
+  registerUser() {
+    this.isUserRegistered.set(true);
+    this.showInitialMessage.set(true);
+
+    setTimeout(() => {
+      this.showInitialMessage.set(false);
+    }, 5000);
+  }
+
+  setGoal() {
+    if(this.dailyStepGoal() > 0) {
+      this.isGoalSet.set(true);
+      this.dailyTargets.push(this.dailyStepGoal());
+    }
   }
 }
