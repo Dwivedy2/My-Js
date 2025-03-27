1e5d1f431cf7d01d1bfb68eca7a1c7c07946a1b7
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-03-27 11:23:27 +0000

    Understanding closures in js

---
+++ JS/10_classes_and_oops/closure.js
@@ -0,0 +1,25 @@
+// closure means, child function has access to parent function properties
+// example
+function parent() {
+    // property
+    let fatherName = "Papa";
+    let motherName = "Mom";
+
+    function baby1() {
+        // child has access
+        // Output : My father name is Papa, and my mother name is Mom
+        let baby1Name = "Brother";
+        console.log(`My father name is ${fatherName}, and my mother name is ${motherName}`);
+    }
+
+    // sibilings cannot access each other property
+    function baby2() {
+        // Error: baby1Name is not defined
+        console.log(`My sibiling is : ${baby1Name}`);
+    }
+
+    baby1();
+    baby2();
+}
+
+parent();
