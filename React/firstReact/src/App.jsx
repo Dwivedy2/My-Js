e0cfa93d86a2d4f8bb0624ee7055d8a3cf5f461f
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-04-02 10:06:34 +0000

    Basic react app for displaying cards

--- React/firstReact/src/App.jsx
+++ React/firstReact/src/App.jsx
@@ -1,35 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+
+  function incCounter() {
+    setCounter(counter + 1);
+  }
+
+  const notes = [
+    {
+      Title: "Making branch as per minh",
+      Description: "Make a branch as per minh suggestion on master"
+    },
+    {
+      Title: "Check story in anshi machine",
+      Description: "Check story in anshi machine and check if it is working"
+    },
+    {
+      Title: "Learn React",
+      Description: "Make some light weight projects in react"
+    },
+  ];
 
   return (
+    // <>
+    //     <h1>Hello React</h1>
+    //     <Button getCounter={counter} handlerFunc={incCounter} />
+    //     <Button getCounter={counter} handlerFunc={incCounter} />
+    // </>
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {notes.map(({Title, Description}) => <Card title={Title} description={Description} />)}
     </>
   )
+}
+
+function Button({getCounter, handlerFunc}) {  
+  return (
+    <>
+      <p>Total Clicks: {getCounter}</p>
+      <button onClick={handlerFunc}>Click Counter</button>
+    </>
+  );
+}
+
+function Card({title, description}) {
+  return (
+    <div class="card">
+      <div class="container">
+        <h4><b>{title}</b></h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App
