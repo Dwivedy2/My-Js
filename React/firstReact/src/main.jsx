7cd581f70894885abafb23dead0f14af21e8f104
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-04-06 10:12:39 +0000

    create element and evaluated expression

--- React/firstReact/src/main.jsx
+++ React/firstReact/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createElement } from 'react'
 import './index.css'
 import App from './App.jsx'
 
@@ -7,9 +8,20 @@ const custom = (
   <a href="https://www.google.com" target='_blank'>Google</a>
 )
 
+function app() {
+  const username = "My Evaluated Expression"
+  return (
+    <a href="https://www.google.com" target='_blank'>Google {username}</a>
+  )
+}
+
+const username = "My Evaluated Expression"
+const a = createElement('a', {href: "https://www.google.com", target: "_blank"}, "Launch Google", username)
+
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     // <App />
-    custom
+    a
+    // app()
   // </StrictMode>,
 )
