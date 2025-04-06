16a26e90a1f19cda863ece0a6f17638d88a684dc
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-04-06 09:57:58 +0000

    custom is a jsx

--- React/firstReact/src/main.jsx
+++ React/firstReact/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const custom = (
+  <a href="https://www.google.com" target='_blank'>Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+    // <App />
+    custom
+  // </StrictMode>,
 )
