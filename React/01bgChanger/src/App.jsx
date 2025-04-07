b14609b782bf4fea0fee0629b99a6dd85bccca24
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-04-07 17:51:13 +0000

    bgChanger with useState hook

--- React/01bgChanger/src/App.jsx
+++ React/01bgChanger/src/App.jsx
@@ -2,17 +2,32 @@ import { useState } from 'react'
 import Button from './components/Button'
 import './App.css'
 
-function App({body}) {
-  const [count, setCount] = useState(0)
-  const colours = ['red', 'green', 'blue', 'olive', 'gray', 'yellow', 'pink', 'purple', 'lavender', 'white', 'black'];
-
-  function bgChange(color) {
-    body.style.backgroundColor = color;
-  }
+function App({ body }) {
+  const [color, setColor] = useState('olive')
 
   return (
-    <div className='w-full p-4 bg-black fixed bottom-10 flex flex-wrap gap-10 justify-center'>
-      {colours.map((colour, index) => <Button key={index} color={colour} handleClick={bgChange} />)}
+    <div className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >Red</button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >Green</button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >Blue</button>
+        </div>
+      </div>
     </div>
   )
 }
